c5cbd559ba0a7d264c6baa802cd71a2c0081f7b3
Zion Gutierrez <zguti001@wch136-16.cs.ucr.edu>
2019-10-03 13:59:47 -0700

    Completed part 1

--- header/simAVRHeader.h
+++ header/simAVRHeader.h
@@ -11,10 +11,11 @@ AVR_MCU_VCD_FILE("build/results/Lab2_introToAVR_trace.vcd",1000);
 
 const struct avr_mmcu_vcd_trace_t _mytrace[] _MMCU_ = {
     { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0,.what = (void*)&PINA, } , // Example individual pin
-    { AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
-    { AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, } ,
-    { AVR_MCU_VCD_SYMBOL("PINA0"), .mask = 1 << 0, .what = (void*)&PINA, }
-
+    { AVR_MCU_VCD_SYMBOL("PINA1"), .mask = 1 << 1, .what = (void*)&PINA, } ,
+    { AVR_MCU_VCD_SYMBOL("PORTB"), .mask = 1 << 0, .what = (void*)&PORTB, }
+  //{ AVR_MCU_VCD_SYMBOL("PORTB"), .what = (void*)&PORTB, } , // Example full port
+  //{ AVR_MCU_VCD_SYMBOL("PINA"), .what = (void*)&PINA, } ,
+    
 };
 
 /* Function to output through UART */
