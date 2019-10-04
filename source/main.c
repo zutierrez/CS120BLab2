3dcd87130d0b0e9fe0f9d6bb885e1ed8451c2b6c
Zion Gutierrez <zguti001@wch136-30.cs.ucr.edu>
2019-10-04 16:44:36 -0700

    Exercise 2, updated

--- source/main.c
+++ source/main.c
@@ -12,25 +12,28 @@
 #include "simAVRHeader.h"
 #endif
 
+#define spot4 (PINA & 0x08) >> 3
+#define spot3 (PINA & 0x04) >> 2
+#define spot2 (PINA & 0x02) >> 1
+#define spot1 (PINA & 0x01) >> 0
+
 int main(void) {
         DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	unsigned char inputA = 0x00; // Temp (PORTA, bits represent spaces)
-    	unsigned char cntavail = 0x04; //Temp (PORTC, amount of empty spaces)
+    	unsigned char cntavail = 0x00; //Temp (PORTC, amount of empty spaces)
 
 	while (1) {
 	// Input
-	inputA = PINA; //
-	
 
-	if( ((inputA & 0xFE) >> 0) == 1 ){
-		cntavail -= 1;}
-	if ( ((inputA & 0xFD) >> 1) == 1 ){
-		cntavail -=1;}
-	if( ((inputA & 0xFC) >> 2) == 1 ){
-		cntavail -= 1;}
-	if ( ((inputA & 0xFB) >> 3) == 1 ){
-		cntavail -=1;}
+	if( spot4 == 0 ){
+		cntavail += 1; }
+	if( spot3 == 0 ){
+		cntavail += 1;}
+	if( spot2 == 0 ){
+		cntavail += 1;}
+	if( spot1  == 0 ){
+		cntavail += 1;}
 
 	// 3) Write output
         PORTC = cntavail;	
